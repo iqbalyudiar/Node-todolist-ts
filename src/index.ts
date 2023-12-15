660828a26ff9ae57f91ee70ed4ed920b259cf60f
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-15 16:10:01 +0800

    feat: add routes on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/", routes);
+
 const port = 5000;
 
 app.listen(port, () => {
