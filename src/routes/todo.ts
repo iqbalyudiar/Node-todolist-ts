665dcfe7b5a11834912d99b63c30a8d15ee43762
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-15 19:09:38 +0800

    feat: add post todo router

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -3,9 +3,11 @@ import controller from "../controllers";
 
 const router = Router();
 const {
-  todo: { getTodos },
+  todo: { getTodos, addTodo },
 } = controller;
 
 router.get("/todos", getTodos);
+
+router.post("/todo", addTodo);
 
 export default router;
