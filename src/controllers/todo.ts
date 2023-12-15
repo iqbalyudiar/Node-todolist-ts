8de3941ffc5aaaac9d153a6c8c8e056d2feebf69
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-15 19:09:23 +0800

    feat: add addTodo controller

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { Todo } from "../models";
+
 export const getTodos = (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json([
     {
@@ -10,4 +12,27 @@ export const getTodos = (req: Request, res: Response, next: NextFunction) => {
   ]);
 };
 
-export default { getTodos };
+export const addTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title, description } = req.body;
+  const todo = new Todo({
+    title,
+    description,
+    creator: "Iqbal",
+    status: "todo",
+  });
+  try {
+    const result = await todo.save();
+    res.status(201).json({
+      message: "Todo created",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { getTodos, addTodo };
