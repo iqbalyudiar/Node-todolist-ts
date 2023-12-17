29eb9c4b970c3748011abfc6ce9f2797945b1526
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-17 19:50:53 +0800

    feat: add password to user models

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,6 +5,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
