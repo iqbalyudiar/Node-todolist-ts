8a10950b7542322d462721e80d56c5bf0718fd36
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-19 13:14:12 +0800

    feat: add middleware validation

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+import isAuth from "./is-auth";
+import * as authValidation from "./authValidation";
+import errorValidation from "./errorValidation";
+
+export { isAuth, authValidation, errorValidation };
