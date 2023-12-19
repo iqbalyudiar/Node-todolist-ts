94ff56b7fd08225deae3b43967fd6bc466d349e3
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-19 13:14:30 +0800

    feat: add validation on auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import controller from "../controllers";
-
+import { authValidation, errorValidation } from "../middleware";
 const router = Router();
 
 const {
   auth: { signup, signin },
 } = controller;
 
+const { signupRules } = authValidation;
+
 // signup
-router.post("/signup", signup);
+router.post("/signup", signupRules, errorValidation, signup);
 
 // signin
 router.post("/signin", signin);
