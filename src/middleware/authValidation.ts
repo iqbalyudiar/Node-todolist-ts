2f3f108fff093d7beaaf83ef9adf063ed02b7f2b
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-19 13:13:19 +0800

    feat: add authValidation

---
+++ src/middleware/authValidation.ts
@@ -0,0 +1,14 @@
+import { body } from "express-validator";
+import { User } from "../models";
+export const signupRules = [
+  body("username").custom(async (username) => {
+    const user = await User.findOne({ username });
+    if (user) {
+      const error = new Error("User already exists");
+      throw error;
+    }
+    return true;
+  }),
+  body("password").trim().isLength({ min: 5 }),
+  body("name").trim().not().isEmpty(),
+];
