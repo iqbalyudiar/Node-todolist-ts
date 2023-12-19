a6b996f36af3b7875e2cd9bf6f1a1fb51ba587be
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-19 13:33:02 +0800

    feat: add errorHandler utils on index ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import routes from "./routes";
-import mongoose from "mongoose";
 
-import { dbConnect } from "./utils";
+import { dbConnect, errorHandler } from "./utils";
 
 const app = express();
 app.use(express.json());
 
 app.use("/", routes);
+app.use(errorHandler);
 
 const port = 5000;
 
