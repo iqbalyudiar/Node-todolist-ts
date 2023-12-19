3e872b53cbbdec7a8ca3efd274520d0449fc3c9e
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-12-19 13:34:44 +0800

    feat: add errorHandler default value

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,18 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import { IError } from "../interfaces";
-// export default (
-//   error: IError,
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   console.log(error);
-//   // console.log(error);
-//   // const status = error.statusCode || 500;
-//   // const message = error.message;
-//   // const { data } = error;
-//   // res.status(status).json({ message, data });
-// };
 
 export const errorHandler = (
   error: IError,
@@ -22,6 +9,6 @@ export const errorHandler = (
 ) => {
   const status = error.statusCode || 500;
   const message = error.message;
-  const { data } = error;
+  const { data = [] } = error;
   res.status(status).json({ message, data });
 };
